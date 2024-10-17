86d6696ebada4eb98f1d6bd55abdc69039f222f5
arvind7600 <arvind@bluesheets.io>
2024-10-17 23:20:32 +0530

    astro home page name change

--- app/src/main/java/com/astro/composable/component/AstroHomePageComposble.kt
+++ app/src/main/java/com/astro/composable/component/AstroHomePageComposble.kt
@@ -1,37 +1,18 @@
 package com.astro.composable.component
 
-import android.annotation.SuppressLint
 import android.app.DatePickerDialog
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.graphics.Color.Companion.Cyan
-import androidx.compose.ui.graphics.Color.Companion.LightGray
-import androidx.compose.ui.graphics.Color.Companion.Yellow
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.astro.data.AstroResponse
-import com.astro.data.User
-import com.astro.model.AstroViewModel
-import kotlinx.coroutines.launch
-import java.util.Calendar
 
 @Composable
 fun UserInputField(value: String, onValueChange: (String) -> Unit) {
